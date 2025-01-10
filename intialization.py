a452bc6d7857228c3457edbeeb308636c3e77d74
skalyan11 <skalyan@umich.edu>
2025-01-10 17:49:21 -0500

    layer_data passing

---
+++ intialization.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+X = [[1,2,3,2.5],[2.0,5.0,-1.0,2.0],[-1.5,2.7,3.3,-0.8]]
+
+class Layer_Dense:
+    def _init_(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros(1, n_neurons)
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5,2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+    
+
